caa7ea8146b27a3ed71d1939d4de6de4a5e8b402
alexeykapp <alexeikapp@yandex.ru>
2024-05-28 02:53:01 +0700

    Сделал сервисы, работаю над Header

--- AltayChillPlace/AltayChillPlace/ApiResponses/AdditionalServiceResponse.cs
+++ AltayChillPlace/AltayChillPlace/ApiResponses/AdditionalServiceResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace AltayChillPlace.ApiResponses
+﻿using Newtonsoft.Json;
+
+namespace AltayChillPlace.ApiResponses
 {
     public class AdditionalServiceResponse
     {
+        [JsonProperty("id_additional_service")]
         public int IdAdditionalService { get; set; }
+        [JsonProperty("name_of_additional_service")]
         public string NameOfAdditionalService { get; set; }
+        [JsonProperty("additional_service_price")]
         public int AdditionalServicePrice { get; set; }
+        [JsonProperty("description_of_additional_service")]
         public string DescriptionOfAdditionalService { get; set; }
     }
 }
